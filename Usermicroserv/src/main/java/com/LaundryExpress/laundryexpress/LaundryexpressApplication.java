690d606f21a40c00a369e89fa069abce4198e0f3
collinsln <collangat@gmail.com>
2024-08-20 17:25:47 +0300

    microservice

--- Usermicroserv/src/main/java/com/LaundryExpress/laundryexpress/LaundryexpressApplication.java
+++ Usermicroserv/src/main/java/com/LaundryExpress/laundryexpress/LaundryexpressApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.LaundryExpress.laundryexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class LaundryexpressApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LaundryexpressApplication.class, args);
 	}
 
 }
--- Usermicroserv/src/test/java/com/LaundryExpress/laundryexpress/LaundryexpressApplicationTests.java
+++ Usermicroserv/src/test/java/com/LaundryExpress/laundryexpress/LaundryexpressApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.LaundryExpress.laundryexpress;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class LaundryexpressApplicationTests {
 
 	@Test
 	void contextLoads() {
