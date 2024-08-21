c1476023a28d78b93d34adac1b90823a3cd6ad48
collinsln <collangat@gmail.com>
2024-08-21 09:09:31 +0300

    strated a fresh

---
+++ LaundryExpress/Househelp/src/main/java/com/LaundryHousehelp/HouseHelp/HouseHelpApplication.java
@@ -0,0 +1,13 @@
+package com.LaundryHousehelp.HouseHelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseHelpApplication.class, args);
+	}
+
+}
---
+++ LaundryExpress/Househelp/src/test/java/com/LaundryHousehelp/HouseHelp/HouseHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaundryHousehelp.HouseHelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
